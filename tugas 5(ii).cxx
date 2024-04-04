13c3c556880eb772da3f261ad560e0e99a9cdf34
MutiaGusmawarni <163378768+MutiaGusmawarni@users.noreply.github.com>
2024-04-04 14:59:22 +0700

    Add files via upload

---
+++ tugas 5(ii).cxx
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main () {
+	int jumlahdor;
+	for (int i = 1; i <= 100; i++){
+			if ( i%3 == 0) {cout<<"DOR"<<" ";}
+			else {if (i%5 == 0) {cout<<"DOR"<<" ";}
+			else {jumlahdor++;cout<<i<<" ";}
+			{cout<<"\t";i++;}}
+		}
+		
+	
+		
+	
+}
