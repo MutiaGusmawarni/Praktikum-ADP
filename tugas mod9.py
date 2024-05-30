c6973146e2fc3d651d08e9c8b838d75382ad88f2
MutiaGusmawarni <163378768+MutiaGusmawarni@users.noreply.github.com>
2024-05-30 15:57:26 +0700

    Add files via upload

---
+++ tugas mod9.py
@@ -0,0 +1,62 @@
+import os
+from termcolor import cprint
+
+os.system('cls')
+
+for i in range(1):
+	print(' '*15, end='')
+	cprint('^', 'light_red', 'on_black')
+for i in range(2):
+	print(' '*15, end='')
+	cprint(' ', 'white', 'on_light_grey')
+for i in range (1):
+	print(' '*6, end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red')
+for i in range(5):
+	print(' '*6, end='')
+	cprint(' '*18, 'white', 'on_magenta')
+for i in range(1):
+	print(' '*3, end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white')
+for i in range(5):
+	print(' '*3, end='')
+	cprint(' '*24, 'white', 'on_cyan')
+for i in range (1):
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red', end='')
+	cprint('*'*2, 'red', 'on_white', end='')
+	cprint('*'*2, 'white', 'on_red')
+for i in range (5):
+	cprint(' '*30, 'white', 'on_magenta')
+	
+	
